34fc99bda75c82893fd75c67b678c76b5de76abb
Pooolg <59968996+Pooolg@users.noreply.github.com>
2021-01-09 01:06:11 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -7,38 +7,51 @@ from vk_api import VkUpload
 import requests
 from bs4 import BeautifulSoup
 import time
-login, password = 'ЛогинВК', 'ПарольВк'
-app_id = '7719267'
-vk_session = vk_api.VkApi(login, password, app_id)       #можно использовать авторизацию через токен приложения, читать документацию vk_api
+
+
+login, password = 'ЛогинВк', 'ПарольВК'
+channel = "КаналТГбез@"
+owner_id = "ВашЧисловойIDвк"
+text = "ТекстСзаписью,например#anime"
+
+vk_session = vk_api.VkApi(login, password)
 vk_session.auth()
-g=0
-while g == 0:
-    url = 'https://rsshub.app/telegram/channel/sad_art_gallery'     #Вместо "sad_art_gallery" вписывайте любой публичный канал, откуда нужно сграбить пикчи
+lastnum = 0
+while True:
+    url = 'https://rsshub.app/telegram/channel/'+channel
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, features='xml')
-    items = soup.find_all('item')
-    score = 0
+    items = soup.find('item')
     upload = VkUpload(vk_session)
-    for i in range(len(items)):
-        item = items[i]
-        l = str(item.description)
-        URL = l[l.find('"') + 1:l.find('jpg') + 3]
-        score = score + 1
-        filename = 'pic/jk' + str(score) + '.jpg'
-        response = requests.get(URL)
-        if response.status_code == 200:
-            with open(filename, 'wb') as imgfile:
-                imgfile.write(response.content)
-        directory = 'pic//'
-        files = os.listdir(directory)
-        files = [i for i in files if i.endswith('.jpg')]
-        photos = (directory + random.choice(files))
-        photo_list = upload.photo_wall(photos)
-        attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
-        vk_session.method("wall.post", {
-            'owner_id': '-193504246',            # id вашего сообщества в ВКонтакте
-            'message': '#anime',                 # текст поста
-            'attachment': attachment,
-        })
-        os.remove(photos)
-        time.sleep(3600)                         № интервал между публикациями
+    lastcount = str(items.contents[9].next)[str(items.contents[9].next).find(channel)+len(channel)+1:]
+    print(lastcount)
+    try:
+        if lastnum != lastcount:
+            lastnum = lastcount
+            item = items
+            l = str(item.description)
+            URL = l[l.find('"') + 1:l.find('jpg') + 3]
+            filename = 'pic/jk.jpg'
+            response = requests.get(URL)
+            if response.status_code == 200:
+                with open(filename, 'wb') as imgfile:
+                    imgfile.write(response.content)
+            directory = 'pic//'
+            files = os.listdir(directory)
+            files = [i for i in files if i.endswith('.jpg')]
+            photos = (directory + random.choice(files))
+            photo_list = upload.photo_wall(photos)
+            attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
+            vk_session.method("wall.post", {
+                'owner_id': '-' + owner_id,
+                'message': text,
+                'attachment': attachment,
+            })
+            os.remove(photos)
+            print("Отправил новое фото")
+        else:
+            print("Нет новых фото в rss")
+        time.sleep(3600)
+    except LookupError:
+        print("Нет пикч")
+        time.sleep(3600)
